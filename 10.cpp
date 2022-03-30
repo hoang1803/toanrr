1be9b205126a944294173cf91830ff60a3140f7b
hoang1803 <21020073@vnu.edu.vn>
2022-03-30 16:55:11 +0700

    ex10

---
+++ 10.cpp
@@ -0,0 +1,42 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define task "10"
+#define MASK(i) (1LL << (i))
+#define c_bit(i) __builtin_popcountll(i)
+#define BIT(x, i) ((x) & MASK(i))
+#define SET_ON(x, i) ((x) | MASK(i))
+#define SET_OFF(x, i) ((x) & ~MASK(i))
+#define all(x) x.begin(), x.end()
+// const int MAXN = ;
+const int INF = 1e9 + 7;
+long long gcd(long long a, long long b)
+{
+    if(a == 0 && b == 0)
+    {
+        cout << "Error";
+        exit(0);
+    }
+    if(a > b)
+        return gcd(b, a);
+    if(a == 0)
+        return b;
+    if(a % 2 == 0 && b % 2 == 0)
+        return 2 * gcd(a / 2, b / 2);
+    if(a % 2 == 0 && b % 2 == 1)
+        return gcd(a / 2, b);
+    return gcd(a, b - a);    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    long long a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << " " << __gcd(a, b);
+    return 0;
+}
